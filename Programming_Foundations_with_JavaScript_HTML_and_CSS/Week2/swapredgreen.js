7d15624391f692c6ccbae7b163a2c1ae08768f96
Iván E. Hernández T <iamieht@gmail.com>
2020-06-14 14:28:59 +0200

    Create swapredgreen.js
    Week2 - SwapRedGreen function

---
+++ Programming_Foundations_with_JavaScript_HTML_and_CSS/Week2/swapredgreen.js
@@ -0,0 +1,34 @@
+var img_hilton = new SimpleImage("hilton.jpg");
+var img_hilton_width = img_hilton.getWidth();
+var img_hilton_third = img_hilton_width / 3;
+// Print original image
+print(img_hilton)
+
+for (var pixel of img_hilton.values() ) {
+    if (pixel.getX() < img_hilton_third) {
+        pixel.setRed(255);
+    } else if (pixel.getX() < img_hilton_third * 2) {
+        pixel.setGreen(255);
+    } else {
+        pixel.setBlue(255);
+    }
+}
+
+//Print modified image
+print(img_hilton)
+
+//SwapRedGreen
+
+function SwapRedGreen(pixel) {
+    var red = pixel.getRed();
+    var green = pixel.getGreen();
+    pixel.setRed(green);
+    pixel.setGreen(red);
+    return pixel;
+}
+
+for (var pixel of img_hilton.values()) {
+    SwapRedGreen(pixel);
+}
+
+print(img_hilton)
