f503fa89cc812bd6e3040a94494a99513c3b675a
Iván E. Hernández T <iamieht@gmail.com>
2020-06-28 08:13:44 +0200

    Week3 - Exercise 3

---
+++ Programming_Foundations_with_JavaScript_HTML_and_CSS/Week3/Exercise3/README.markdown
@@ -0,0 +1,5 @@
+# Exercise3
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/iamieht/pen/bGErZmQ](https://codepen.io/iamieht/pen/bGErZmQ).
+
+
---
+++ Programming_Foundations_with_JavaScript_HTML_and_CSS/Week3/Exercise3/src/script.js
@@ -0,0 +1,26 @@
+function doBlue() {
+  var canv1 = document.getElementById("div1");
+  var context = canv1.getContext("2d");
+  context.clearRect(0,0,canv1.width,canv1.height);
+  canv1.style.backgroundColor = "blue"; 
+  
+}
+
+function doOrange() {
+  var canv1 = document.getElementById("div1");
+  canv1.style.backgroundColor = "orange";
+  var context = canv1.getContext("2d");
+  context.fillStyle = "yellow";
+  context.fillRect(10,10,60,60);
+  context.fillRect(80,10,60,60);
+  context.fillStyle = "black";
+  context.font = "20px Arial";
+  context.fillText("Hello", 15, 45);
+  
+}
+
+function doFuchsia() {
+  var canv2 = document.getElementById("div2");
+  canv2.style.backgroundColor = "fuchsia";
+}
+
