6d31ed8c5462e8207e91691929297cd830f3d0fe
Iván E. Hernández T <iamieht@gmail.com>
2020-10-08 16:39:43 +0200

    Part 3: Problem Solving with Strings
    method named twoOccurrences

---
+++ 2.Java_Programming_Solving_Problems_with_Software/Week2/StringsFirstAssignments/Part3.java
@@ -0,0 +1,47 @@
+
+/**
+ * Write a description of Part3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Part3 {
+    
+    public Boolean twoOccurrences(String stringa, String stringb) {
+    
+        int startIndex = stringb.indexOf(stringa);
+        int stopIndex = stringb.indexOf(stringa, startIndex + stringa.length());
+        Boolean result;
+        if (startIndex == -1 || stopIndex == -1) {
+            result = false;
+        } else {
+            result = true;
+        }
+        return result;
+    }
+    
+    public void testing() {
+    
+        String stringa = "by";
+        System.out.println("Stringa is " + stringa);
+        String stringb = "A story by Abby Long";
+        System.out.println("Stringb is " + stringb);
+        Boolean isStringaInStringbTwice = twoOccurrences(stringa, stringb);
+        System.out.println("isStringaInStringbTwice " + isStringaInStringbTwice);
+        
+        stringa = "a";
+        System.out.println("Stringa is " + stringa);
+        stringb = "banana";
+        System.out.println("Stringb is " + stringb);
+        isStringaInStringbTwice = twoOccurrences(stringa, stringb);
+        System.out.println("isStringaInStringbTwice " + isStringaInStringbTwice);
+        
+        stringa = "atg";
+        System.out.println("Stringa is " + stringa);
+        stringb = "ctgtatgta";
+        System.out.println("Stringb is " + stringb);
+        isStringaInStringbTwice = twoOccurrences(stringa, stringb);
+        System.out.println("isStringaInStringbTwice " + isStringaInStringbTwice);
+    }
+
+}
