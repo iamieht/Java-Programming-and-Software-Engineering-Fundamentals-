5932e4a65596ebe782bb23c82561071b9c1dc0dc
Iván E. Hernández T <iamieht@gmail.com>
2020-10-15 18:15:30 +0200

    Add missing methods from Part2

--- 2.Java_Programming_Solving_Problems_with_Software/Week2/StringsThirdAssignments/Part3.java
+++ 2.Java_Programming_Solving_Problems_with_Software/Week2/StringsThirdAssignments/Part3.java
@@ -1,4 +1,5 @@
 import edu.duke.StorageResource;
+import edu.duke.FileResource;
 /**
  * Write a description of Part3 here.
  * 
@@ -6,6 +7,53 @@ import edu.duke.StorageResource;
  * @version (a version number or a date)
  */
 public class Part3 {
+    public int findStopCodon(String dna, int startIndex, String stopCodon) {
+    
+        int currIndex = dna.indexOf(stopCodon, startIndex + 3);
+        while (currIndex != -1) {
+            int diff = currIndex - startIndex;
+            if (diff % 3 == 0) {
+                return currIndex;
+            }
+            else {
+                currIndex = dna.indexOf(stopCodon, currIndex + 1);
+            }
+            
+        }
+        return dna.length();
+    }
+    
+    public String findGene(String dna, int where) {
+        int startIndex = dna.indexOf("ATG", where);
+        if (startIndex == -1) {
+            return "";
+        }
+        int taaIndex = findStopCodon(dna,startIndex,"TAA");
+        int tagIndex = findStopCodon(dna,startIndex,"TAG");
+        int tgaIndex = findStopCodon(dna,startIndex,"TGA");
+        int temp = Math.min(taaIndex,tagIndex);
+        int minIndex = Math.min(temp,tgaIndex);
+        if (minIndex == dna.length()) {
+            return "";
+        }
+        return dna.substring(startIndex,minIndex + 3);
+    }
+    
+    public StorageResource getAllGenes(String dna) {
+        StorageResource geneList = new StorageResource();
+        int startIndex = 0;
+        
+        while (true) {
+            String currentGene = findGene(dna, startIndex);
+            
+            if (currentGene.isEmpty()) {
+                break;
+            }
+            geneList.add(currentGene);
+            startIndex = dna.indexOf(currentGene, startIndex) + currentGene.length();
+        }
+        return geneList;
+    }
     
     public void processGenes(StorageResource sr) {
         StorageResource dnaList9 = longerThan(sr, 9);
@@ -63,4 +111,8 @@ public class Part3 {
         processGenes(sr);
     }
     
+    public void testProcessGenesFile() {
+        FileResource fr = new FileResource("brca1line.fa");
+        
+    }
 }
