b083420b4d3aa4dc2f9fec52ca19b9e755e20874
Iván E. Hernández T <iamieht@gmail.com>
2020-10-23 18:24:42 +0200

    Week3 - Countries example

---
+++ 2.Java_Programming_Solving_Problems_with_Software/Week3/WhichCountriesExport/WhichCountriesExport.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of WhichCountriesExport here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import org.apache.commons.csv.*;
+public class WhichCountriesExport {
+    
+    public void listExporters(CSVParser parser, String exportOfInterest) {
+        for(CSVRecord record : parser) {
+            String export = record.get("Exports");
+            if (export.contains(exportOfInterest)) {
+                String country = record.get("Country");
+                System.out.println(country);
+            }
+        }
+    }
+    public void whoExportsCoffee() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        listExporters(parser, "coffee");
+    }
+
+}
