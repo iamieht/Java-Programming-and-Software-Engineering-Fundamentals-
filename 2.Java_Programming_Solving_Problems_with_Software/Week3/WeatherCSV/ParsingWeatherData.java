6aaf7af93c7ce4791322e21d83e523b146a3f2c1
Iván E. Hernández T <iamieht@gmail.com>
2020-10-25 10:44:50 +0100

    fileWithColdestTemperature

--- 2.Java_Programming_Solving_Problems_with_Software/Week3/WeatherCSV/ParsingWeatherData.java
+++ 2.Java_Programming_Solving_Problems_with_Software/Week3/WeatherCSV/ParsingWeatherData.java
@@ -28,18 +28,54 @@ public class ParsingWeatherData {
             double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
             double smallestTemp = Double.parseDouble(smallestSoFar.get("TemperatureF"));
             
-            if (currentTemp < smallestTemp) {
+            if (currentTemp != -9999 && currentTemp < smallestTemp) {
                 smallestSoFar = currentRow;
             }
         }
         return smallestSoFar;
     }
     
+    public String fileWithColdestTemperature() {
+        String filename = "";
+        CSVRecord smallestSoFar = null;
+        DirectoryResource dr = new DirectoryResource();
+        
+        for (File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            CSVRecord currentRow = coldestHourInFile(fr.getCSVParser());
+            if (smallestSoFar == null) {
+                smallestSoFar = currentRow;
+                filename = f.toString();
+            }
+            else {
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                double smallestTemp = Double.parseDouble(smallestSoFar.get("TemperatureF"));
+                if (currentTemp != -9999 && currentTemp < smallestTemp) {
+                    smallestSoFar = currentRow;
+                    filename = f.toString();
+                }
+            }
+        }
+        return filename;
+    }
+    
+   
     public void testColdestHourInFile() {
         FileResource fr = new FileResource();
         CSVRecord coldest = coldestHourInFile(fr.getCSVParser());
-        System.out.println("coldest temperature was " + coldest.get("TemperatureF") +
+        System.out.println("Coldest temperature was " + coldest.get("TemperatureF") +
                             "F" + " at " + coldest.get("TimeEST"));
     }
-
+    
+    public void testFileWithColdestTemperature() {
+        String filename = fileWithColdestTemperature();
+        System.out.println("File with coldest temperature is " + filename);
+        FileResource fr = new FileResource(filename);
+        CSVRecord coldest = coldestHourInFile(fr.getCSVParser());
+        System.out.println("Coldest temperature on that day was " + coldest.get("TemperatureF"));
+        System.out.println("All the Temperatures on the coldest day were:");
+        for (CSVRecord currentRow : fr.getCSVParser()) {
+            System.out.println( currentRow.get("DateUTC") + ": " + currentRow.get("TemperatureF"));
+        }
+    }
 }
