617910ed70357e6cf8ff686cd08ea0aa1042bb26
Iván E. Hernández T <iamieht@gmail.com>
2020-10-25 08:24:00 +0100

    WeatherCSV Problem
    method hottestInManyDays

--- 2.Java_Programming_Solving_Problems_with_Software/Week3/WeatherCSV/CSVMax.java
+++ 2.Java_Programming_Solving_Problems_with_Software/Week3/WeatherCSV/CSVMax.java
@@ -37,4 +37,31 @@ public class CSVMax {
                             " at " + largest.get("TimeEST"));
     }
     
+    public CSVRecord hottestInManyDays () {
+        CSVRecord largestSoFar = null;
+        DirectoryResource dr = new DirectoryResource();
+        
+        for (File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            CSVRecord currentRow = hottestHourInFile(fr.getCSVParser());
+            if (largestSoFar == null) {
+                largestSoFar = currentRow;
+            }
+            else {
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+                
+                if (currentTemp > largestTemp) {
+                    largestSoFar = currentRow;
+                }
+            }
+        }
+        return largestSoFar;
+    }
+    
+    public void testHottestInManyDays () {
+        CSVRecord largest = hottestInManyDays();
+        System.out.println("hottest temperature was " + largest.get("TemperatureF") +
+                            " at " + largest.get("DateUTC"));
+    }
 }
