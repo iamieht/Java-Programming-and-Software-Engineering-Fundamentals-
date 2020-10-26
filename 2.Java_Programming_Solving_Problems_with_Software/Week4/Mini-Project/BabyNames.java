36c2dfd9f2775bb225113f0797f5d194f48f8b36
Iván E. Hernández T <iamieht@gmail.com>
2020-10-26 20:51:37 +0100

    Update BabyNames.java
    print total girl and boy names

--- 2.Java_Programming_Solving_Problems_with_Software/Week4/Mini-Project/BabyNames.java
+++ 2.Java_Programming_Solving_Problems_with_Software/Week4/Mini-Project/BabyNames.java
@@ -25,21 +25,27 @@ public class BabyNames {
         int totalBoys = 0;
         int totalGirls = 0;
         int totalNames = 0;
+        int totalGirlNames = 0;
+        int totalBoyNames = 0;
         for (CSVRecord record : fr.getCSVParser(false)) {
             int numBorn = Integer.parseInt(record.get(2));
             totalBirths += numBorn;
             totalNames += 1;
             if (record.get(1).equals("M")) {
                 totalBoys += numBorn;
+                totalBoyNames += 1;
             }
             else {
                 totalGirls += numBorn;
+                totalGirlNames += 1;
             }
         }
         System.out.println("Total births = " + totalBirths);
         System.out.println("Total girls = " + totalGirls);
         System.out.println("Total boys = " + totalBoys);
         System.out.println("Total names = " + totalNames);
+        System.out.println("Total girl names = " + totalGirlNames);
+        System.out.println("Total boy names = " + totalBoyNames);
     }
     
     public void testTotalBirths () {
